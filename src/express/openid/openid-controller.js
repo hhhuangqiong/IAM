e730f59f6edc0933b876e55cb854e7b93e4b7c86
Thomas Lee <thomas1lee@maaii.com>
2016-08-19 11:10:49 +0800

    Fix the login error handling
    It will redirect back to the previous page if the id/password in correct.
    
    It will then read from the error code from param and pass it to the login page in the props.error
    
    Code:
    ( missing id/password/not found user)
    ArgumentNullError
    NotFoundError
            code = 20001;
    
    ( incorrect password)
    ValidationError.name:
            code = 20004
    
    (other error)
    default:
            code = 20000;
    
    Change-Id: Iea2dd766ab24605899ae4543fdcf3ef69610ecb2

--- src/express/openid/openid-controller.js
+++ src/express/openid/openid-controller.js
@@ -2,35 +2,82 @@ import { Router } from 'express';
 import wrap from 'co-express';
 import _ from 'lodash';
 import bodyParser from 'body-parser';
+import { NotFoundError, ValidationError, ArgumentNullError } from 'common-errors';
 
 const jsonParser = bodyParser.json();
 const urlencodedParser = bodyParser.urlencoded({
   extended: true,
 });
 
+function removeURLParameter(url, parameter) {
+  const urlparts = url.split('?');
+  if (urlparts.length >= 2) {
+    const prefix = `${encodeURIComponent(parameter)}=`;
+    const pars = urlparts[1].split(/[&;]/g);
+    // reverse iteration as may be destructive
+    for (let i = pars.length; i-- > 0;) {
+      // idiom for string.startsWith
+      if (pars[i].lastIndexOf(prefix, 0) !== -1) {
+        pars.splice(i, 1);
+      }
+    }
+    return urlparts[0] + (pars.length > 0 ? `?${pars.join('&')}` : '');
+  }
+  return url;
+}
+
 export function openIdController(getProvider, userService) {
   const router = new Router();
 
-  function* login(req, res, next) {
-    if (!req.body.id || !req.body.password || !req.body.grant) {
-      res.sendStatus(422);
+  function loginErrorHandler(req, res, error) {
+    let code;
+    switch (error.name) {
+      case ArgumentNullError.name:
+      case NotFoundError.name:
+        code = 20001;
+        break;
+      case ValidationError.name:
+        code = 20004;
+        break;
+      default:
+        code = 20000;
+        break;
+    }
+    // remove the previous error code
+    const url = removeURLParameter(req.get('referer'), 'error');
+    // refer back to previous page with the error code
+    res.redirect(`${url}?error=${code}`);
+  }
+  function* login(req, res) {
+    if (!req.body.id) {
+      loginErrorHandler(req, res, new ArgumentNullError('id'));
       return;
     }
-    const provider = yield getProvider;
-    try {
-      const account = yield provider.get('Account').findByLogin(req.body.id, req.body.password);
-      const result = {
-        login: {
-          account: account.accountId,
-          acr: 1,
-          ts: Date.now() / 1000 | 0,
-          remember: !!req.body.remember,
-        },
-      };
-      provider.resume(res, req.body.grant, result);
-    } catch (ex) {
-      next(ex);
+    if (!req.body.password) {
+      loginErrorHandler(req, res, new ArgumentNullError('password'));
+      return;
     }
+    if (!req.body.grant) {
+      loginErrorHandler(req, res, new ArgumentNullError('grant'));
+      return;
+    }
+
+    const provider = yield getProvider;
+    let account;
+    try {
+      account = yield provider.get('Account').findByLogin(req.body.id, req.body.password);
+    } catch (ex) {
+      loginErrorHandler(req, res, ex);
+    }
+    const result = {
+      login: {
+        account: account.accountId,
+        acr: 1,
+        ts: Date.now() / 1000 | 0,
+        remember: !!req.body.remember,
+      },
+    };
+    provider.resume(res, req.body.grant, result);
   }
 
   function* loginInteraction(req, res) {
@@ -40,12 +87,17 @@ export function openIdController(getProvider, userService) {
       return;
     }
     const grantData = JSON.parse(grant);
-    res.render('signIn', {
+    const result = {
       grant: req.params.grant,
       clientId: grantData.params.client_id,
       redirectURL: grantData.params.redirect_uri,
       details: grantData.details,
-    });
+    };
+    // to read the error message if there is param and pass to render page
+    if (req.query.error) {
+      result.error = req.query.error;
+    }
+    res.render('signIn', result);
   }
 
 
